1973cb6e461e7aed27b2851c1978f28b74546d10
ademola <ademolaowoeye07@gmail.com>
2022-07-30 22:59:40 +0100

    fixing linters

--- about.js
+++ about.js
@@ -1,38 +1,33 @@
-const hamburger = document.getElementsByClassName('HB-container')
-const menu = document.querySelectorAll('#menu1')
-const sections = document.querySelectorAll('section')
-const xmark1 = document.getElementById('x-mark-container')
-const header = document.querySelectorAll('header')
-const footer = document.querySelectorAll('footer')
-const tis = document.querySelectorAll('.logo-reservedright')
+const hamburger = document.getElementsByClassName('HB-container');
+const menu = document.querySelectorAll('#menu1');
+const sections = document.querySelectorAll('section');
+const xmark1 = document.getElementById('x-mark-container');
+const header = document.querySelectorAll('header');
+const footer = document.querySelectorAll('footer');
+const tis = document.querySelectorAll('.logo-reservedright');
 
-   hamburger[0].addEventListener('click', () => {
+hamburger[0].addEventListener('click', () => {
+  for (let i = 0; i < hamburger.length; i += 1) {
+    menu[i].style.display = 'grid';
 
-      for (let i = 0; i < hamburger.length; i += 1 ) {
-       menu[i].style.display = 'grid';
-       
-       for (let i = 0; i < sections.length; i += 1) {     
-         sections[i].style.display = 'none';
-        }
+    for (let i = 0; i < sections.length; i += 1) {
+      sections[i].style.display = 'none';
+    }
 
-        tis[0].style.display='none'
-        header[i].style.display = 'none';
-        footer[i].style.display = 'none';
+    tis[0].style.display = 'none';
+    header[i].style.display = 'none';
+    footer[i].style.display = 'none';
+  }
+});
 
-      }
-   })
+xmark1.addEventListener('click', () => {
+  for (let i = 0; i < hamburger.length; i += 1) {
+    menu[i].style.display = 'none';
 
-   
-   xmark1.addEventListener('click', () => {
-      for (let i = 0; i < hamburger.length; i += 1 ) {
-       
-         menu[i].style.display = 'none';
-         
-         for (let i = 0; i < sections.length; i += 1) {     
-           sections[i].style.display = 'block';
-          }
-          header[i].style.display = 'block';
-        footer[i].style.display = 'block';
-  
-        }
-   })
+    for (let i = 0; i < sections.length; i += 1) {
+      sections[i].style.display = 'block';
+    }
+    header[i].style.display = 'block';
+    footer[i].style.display = 'block';
+  }
+});
